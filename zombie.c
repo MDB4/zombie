ab7f4b84d25c72a422c32b1cdc4d9f109aae4c39
MDB4 <MDB4@users.noreply.github.com>
2021-07-31 18:13:38 -0700

    Update zombie.c

--- zombie.c
+++ zombie.c
@@ -9,8 +9,17 @@ gcc zombie.c -o zombie
 
 int main ( int argc, char *argv[]) {
 
+	if (argc<3) {
+		if (argv[2] = "-w") {
+			child_pid = fork ();
+			if (child_pid == 0) {
+				execve ("watch", "-n 0.333 sudo ps axo stat,ppid,pid,comm | grep -w defunct");
+			}
+		}
+	}
+	
 	if (argc<2) {
-		argv[1] = "15";
+		argv[1] = "9";
 	}
 	
 	char* sleepDur_S = argv[1];
@@ -19,13 +28,13 @@ int main ( int argc, char *argv[]) {
 	pid_t child_pid;
 	child_pid = fork ();
 	if (child_pid > 0) {
-		printf("Child:%d sleeping %ss. . .\n", child_pid, sleepDur_S);
+		printf("Child: %d sleeping %ss. . .\n", child_pid, sleepDur_S);
 		sleep (sleepDur);
 	} else {
-		printf("Parent:%d exiting. . .\n", selfPid);
+		printf("Parent: %d exiting. . .\n", selfPid);
 		exit (0);
 	}
 	
-	printf("Child:%d returning. . .\n", child_pid);
+	printf("Child: %d returning. . .\n", child_pid);
 	return 0;
 }
